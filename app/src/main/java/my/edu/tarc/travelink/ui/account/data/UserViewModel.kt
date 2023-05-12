11c108ef1ca044de319049410880c743cd203a98
Cabbage9042 <xiangwen800@gmail.com>
2023-05-12 20:07:50 +0800

    REAL FINAL VERSION NO MORE ADDING
    no more adding, too tired

--- app/src/main/java/my/edu/tarc/travelink/ui/account/data/UserViewModel.kt
+++ app/src/main/java/my/edu/tarc/travelink/ui/account/data/UserViewModel.kt
@@ -1,8 +1,11 @@
 package my.edu.tarc.travelink.ui.account.data
 
 import android.app.Application
+import android.util.Log
 import androidx.annotation.WorkerThread
+import androidx.databinding.adapters.NumberPickerBindingAdapter.setValue
 import androidx.lifecycle.*
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FieldValue.arrayUnion
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObjects
@@ -10,8 +13,13 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import my.edu.tarc.travelink.R
 import my.edu.tarc.travelink.ui.wallet.data.Trip
+import my.edu.tarc.travelink.ui.wallet.data.TripRepository
 
 class UserViewModel(application: Application) : AndroidViewModel(application) {
+    companion object {
+        val database =
+            Firebase.database("https://travelink-dc333-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
+    }
 
     private val firebaseUser =
         Firebase.firestore.collection("users").document(CURRENT_USER.value!!.email)
@@ -69,7 +77,38 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
     fun updateSeen(newsID: Int){
         CURRENT_USER.value!!.seenPost.add(newsID)
         firebaseUser.collection("seenNews").document(newsID.toString()).set({})
+        readSeen()
+        addNewsCountIntoRTDB(newsID)
     }
 
-    //TODO: Read seen from database, update seenCount to RTdatabase
+    @WorkerThread
+    fun readSeen(){
+        firebaseUser.collection("seenNews").get().addOnSuccessListener{documents->
+
+            //get the seen news id list
+            val seenList = mutableListOf<Int>()
+            documents.forEach {doc->
+                seenList.add(doc.id.toInt())
+            }
+
+            //set it into current user
+            CURRENT_USER.value!!.seenPost = seenList
+
+        }
+    }
+
+    @WorkerThread
+    fun addNewsCountIntoRTDB(newsID: Int){
+        //get clicked count
+        database.child("news").child(newsID.toString()).child("userViewed").get().addOnSuccessListener {
+
+            if (!it.exists()) {
+                database.child("news").child(newsID.toString()).child("userViewed").setValue(1)
+                return@addOnSuccessListener
+            }
+
+            val value = it.value.toString().toInt()
+            database.child("news").child(newsID.toString()).child("userViewed").setValue(value+ 1)
+        }
+    }
 }
